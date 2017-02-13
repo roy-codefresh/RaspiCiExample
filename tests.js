309d5283fbf08d6827346d8cce0a8aab9ce8ae0a
roy <roy@codefresh.io>
2017-02-13 11:35:21 +0200

    Change tests

--- tests.js
+++ tests.js
@@ -1,23 +1,21 @@
 'use strict';
 
-let request = require('request');
+let request = require('request-json');
 let expect  = require('expect');
+
+let ledControl = request.createClient('http://led-control:3000/');
+let gpioSimulator = request.createClient('http://gpio-simulator:4500/');
 
 describe('LED Application', () => {
 
     it('turn the led off', (done) => {
-        request.post('http://led-control:3000/led', {
-            json: true,
-            body: {
-                value: 1
-            }
-        }, err => {
+        ledControl.post('led', { value:  1 }, err => {
             if (err) {
                 done(err);
                 return;
             }
 
-            request.get('http://gpio-simulator:4500/out/2', { json: true }, (err, res, value) => {
+            gpioSimulator.get('out/2', (err, res, value) => {
                 if (err) {
                     done(err);
                     return;
@@ -29,18 +27,13 @@ describe('LED Application', () => {
     });
     
     it('turn the led on', (done) => {
-        request.post('http://led-control:3000/led', {
-            json: true,
-            body: {
-                value: 0
-            }
-        }, err => {
+        ledControl.post('led', { value: 0 }, err => {
             if (err) {
                 done(err);
                 return;
             }
 
-            request.get('http://gpio-simulator:4500/out/2', { json: true }, (err, res, value) => {
+            gpioSimulator.get('out/2', (err, res, value) => {
                 if (err) {
                     done(err);
                     return;

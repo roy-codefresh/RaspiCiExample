def2a53754ec80efde13985d619182edeadd37c7
roy <roy@codefresh.io>
2017-02-13 09:48:29 +0200

    adding tests

---
+++ tests.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let request = require('request');
+let expect  = require('expect');
+
+describe('LED Application', () => {
+
+    it('turn the led off', (done) => {
+        request.post('http://led-control:3000/led', {
+            json: true,
+            body: {
+                value: 1
+            }
+        }, err => {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            request.get('http://gpio-simulator:4500/out/2', { json: true }, (err, value) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+
+                expect(value).toEqual(1)
+            })
+        });
+    });
+    
+    it('turn the led on', (done) => {
+        request.post('http://led-control:3000/led', {
+            json: true,
+            body: {
+                value: 0
+            }
+        }, err => {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            request.get('http://gpio-simulator:4500/out/2', { json: true }, (err, value) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+
+                expect(value).toEqual(0)
+            })
+        });
+    });
+    
+});
+
